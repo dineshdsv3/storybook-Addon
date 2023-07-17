437e43280f6fc8f3effeff63c7983f00be2763f8
dineshdsv3 <dineshdsv3@gmail.com>
2023-07-18 00:56:22 +0530

    Addon configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
+    './addon/register',
   ],
   framework: '@storybook/react',
   core: {
