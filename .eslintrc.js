46f363fa8fe22826be72ef0b47f31688b8faf22b
dineshdsv3 <dineshdsv3@gmail.com>
2023-07-22 23:57:11 +0530

    ESlint setup and turn-off rule

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'jsx-a11y/media-has-caption': [0],
+    // 0 - off
+    // 1- Warn
+    // 2- Error
+  },
+};
